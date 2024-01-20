a99e8a3b06ef4a6e34467bdb9c5ab554c634ce3c
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-01-20 19:17:53 -0300

    remove build directories from tracking

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nickplace/CLionProjects/my_key_value_db
-# Build directory: /Users/nickplace/CLionProjects/my_key_value_db/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Google_Tests]=] "/Users/nickplace/CLionProjects/my_key_value_db/cmake-build-debug/Google_tests/Google_Tests_run")
-set_tests_properties([=[Google_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/nickplace/CLionProjects/my_key_value_db/CMakeLists.txt;15;add_test;/Users/nickplace/CLionProjects/my_key_value_db/CMakeLists.txt;0;")
-subdirs("Google_tests")
--- cmake-build-debug/Google_tests/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nickplace/CLionProjects/my_key_value_db/Google_tests/lib/googletest/googlemock
-# Build directory: /Users/nickplace/CLionProjects/my_key_value_db/cmake-build-debug/Google_tests/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
