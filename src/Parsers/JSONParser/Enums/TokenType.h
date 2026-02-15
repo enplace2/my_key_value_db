830d7aa46a44ee2789109a0b3f065191924580cd
Nick Place <114956247+enplace2@users.noreply.github.com>
2026-02-14 20:57:39 -0600

    implement JSON tokenizer with string and key parsin
    Add Token struct and TokenVector type
      - Implement JSONTokenizer class with cursor-based parsing
      - Add string and key token generation methods
      - Update build configuration for tokenizer files

--- src/Parsers/JSONParser/Enums/TokenType.h
+++ src/Parsers/JSONParser/Enums/TokenType.h
@@ -4,12 +4,14 @@
 
 #ifndef MY_KEY_VALUE_DB_TOKENTYPE_H
 #define MY_KEY_VALUE_DB_TOKENTYPE_H
+#include "vector"
 enum class TokenType {
     KEY,
-    COLON,
+    COLON = ':',
     STRING,
     BOOLEAN,
-    COMMA,
+    COMMA = ',',
+    EOL,
 };
 
 const std::vector<TokenType> VALUE_TYPES = {TokenType::STRING, TokenType::BOOLEAN};
